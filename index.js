0d60c371ef02f42b4cef9596663ce1e945df5ec4
Lucien  Dulac <lucien.dulac@ekino.com>
2018-03-13 11:16:46 +0100

    modification path to front

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express');
-
+const front = require('./heroku-front-app/public/index.html')
 const app = express();
 
 app.get('*', (req, res) => {
-  res.send('./heroku-front-app')
+  res.send(front);
 })
 
 const port = process.env.PORT || 3000;
