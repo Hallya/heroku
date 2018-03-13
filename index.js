fc2fe1bfdd1eea3f28cfaed396c9d0c16d6bc24d
Lucien  Dulac <lucien.dulac@ekino.com>
2018-03-13 11:14:45 +0100

    initialisation front

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('*', (req, res) => {
-  res.send('hey hey hey !!')
+  res.send('./heroku-front-app')
 })
 
 const port = process.env.PORT || 3000;
